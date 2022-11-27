3b7c30aa32e248cbc44ea52d89206baf8d32922c
MaestroFox <23360120+KakaoC@users.noreply.github.com>
2022-11-27 17:16:11 +0400

    Added Temp files + started new task

---
+++ main_window.py
@@ -0,0 +1,9 @@
+import sys, random
+from genericpath import isfile
+
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtGui import QPixmap, QFont, QIcon
+from PyQt5.QtCore import QThread, QObject
+
+import another_copy
